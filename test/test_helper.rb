f15440bf7c912e767402d8d1c7898dd8090be758
qinmingyuan <mingyuan0715@foxmail.com>
2016-12-28 15:49:14 +0800

    test run

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,18 +1,11 @@
-# Configure Rails Environment
-ENV["RAILS_ENV"] = "test"
+ENV['RAILS_ENV'] ||= 'test'
+require File.expand_path('../../test/dummy/config/environment.rb',  __FILE__)
+require 'rails/test_help'
+require 'minitest/mock'
 
-require File.expand_path("../../test/dummy/config/environment.rb",  __FILE__)
-ActiveRecord::Migrator.migrations_paths = [File.expand_path("../../test/dummy/db/migrate", __FILE__)]
-require "rails/test_help"
-
-# Filter out Minitest backtrace while allowing backtrace from other libraries
-# to be shown.
+ActiveRecord::Migrator.migrations_paths = [File.expand_path('../../test/dummy/db/migrate', __FILE__)]
 Minitest.backtrace_filter = Minitest::BacktraceFilter.new
 
-# Load support files
-Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each { |f| require f }
-
-# Load fixtures from the engine
-if ActiveSupport::TestCase.respond_to?(:fixture_path=)
-  ActiveSupport::TestCase.fixture_path = File.expand_path("../fixtures", __FILE__)
+class ActiveSupport::TestCase
+  include FactoryGirl::Syntax::Methods
 end
