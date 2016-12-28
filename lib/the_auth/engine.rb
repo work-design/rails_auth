8bb7364915763a513d672bb91487fa68493ea7d8
qinmingyuan <mingyuan0715@foxmail.com>
2016-12-28 15:29:38 +0800

    config generator ok

--- lib/the_auth/engine.rb
+++ lib/the_auth/engine.rb
@@ -2,6 +2,11 @@ class TheAuth::Engine < Rails::Engine
   engine_name 'the_auth'
 
   config.generators do |g|
+    g.rails = {
+      assets: false,
+      stylesheets: false,
+      helper: false
+    }
     g.test_unit = {
       fixture: true,
       fixture_replacement: :factory_girl
