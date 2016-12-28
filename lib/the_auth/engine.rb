deb10bef4c137f4d7e110b5e34a0bf54b0365846
qinmingyuan <mingyuan0715@foxmail.com>
2016-12-28 14:25:13 +0800

    update config

--- lib/the_auth/engine.rb
+++ lib/the_auth/engine.rb
@@ -1,11 +1,13 @@
 class TheAuth::Engine < Rails::Engine
   engine_name 'the_auth'
-
-  config.generators do |g|
-    g.assets false
-    g.stylesheets false
-    g.helper false
-    g.fixture_replacement :factory_girl
+  
+  initializer 'the_auth.generator' do |app|
+    app.config.generators do |g|
+      g.assets false
+      g.stylesheets false
+      g.helper false
+      g.fixture_replacement :factory_girl
+    end
   end
 
 end
