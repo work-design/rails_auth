e9f99a06c788a879f695c34a9d40eca9dccbb8f6
qinmingyuan <mingyuan0715@foxmail.com>
2016-12-28 15:26:03 +0800

    update config

--- Gemfile
+++ Gemfile
@@ -1,6 +1,8 @@
 source 'https://rubygems.org'
 gemspec
 
+gem 'pry-rails'
+gem 'byebug'
 gem 'factory_girl_rails'
 
 
--- lib/the_auth/engine.rb
+++ lib/the_auth/engine.rb
@@ -1,13 +1,11 @@
 class TheAuth::Engine < Rails::Engine
   engine_name 'the_auth'
-  
-  initializer 'the_auth.generator' do |app|
-    app.config.generators do |g|
-      g.assets false
-      g.stylesheets false
-      g.helper false
-      g.fixture_replacement :factory_girl
-    end
+
+  config.generators do |g|
+    g.test_unit = {
+      fixture: true,
+      fixture_replacement: :factory_girl
+    }
   end
 
 end
