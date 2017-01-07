ea910d4003bafdfcccdc2a67260eb9b1ed93d814
qinmingyuan <mingyuan0715@foxmail.com>
2017-01-07 16:26:51 +0800

    code style

--- app/controllers/the_auth/login_controller.rb
+++ app/controllers/the_auth/login_controller.rb
@@ -30,7 +30,7 @@ class TheAuth::LoginController < TheAuth::BaseController
   end
 
   def inc_ip_count
-    Rails.cache.write "login/#{request.remote_ip}", ip_count + 1, :expires_in => 60.seconds
+    Rails.cache.write "login/#{request.remote_ip}", ip_count + 1, expires_in: 60.seconds
   end
 
   def ip_count
