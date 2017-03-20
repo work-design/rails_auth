268810262b88299520c0e8fbc5dfc69d61c67c61
qinmingyuan <mingyuan0715@foxmail.com>
2017-03-20 09:52:31 +0800

    update join methods

--- app/controllers/the_auth/join_controller.rb
+++ app/controllers/the_auth/join_controller.rb
@@ -7,7 +7,7 @@ class TheAuth::JoinController < TheAuth::BaseController
 
   def create
     @user = User.new(user_params)
-    if @user.join
+    if @user.join(params)
       login_as @user
       redirect_back_or_default
     else
@@ -28,9 +28,8 @@ class TheAuth::JoinController < TheAuth::BaseController
     ip_count >= 3
   end
 
-  private
   def user_params
-    params.fetch(:user, {}).permit!
+    params.fetch(:user, {}).permit(:name, :email, :password, :password_confirmation)
   end
 
 end
