deddcaf7652e4f63d976d594b25a9cf8f34aa830
qinmingyuan <mingyuan0715@foxmail.com>
2017-04-10 10:53:56 +0800

    remove search helper first

--- app/helpers/the_auth_helper.rb
+++ app/helpers/the_auth_helper.rb
@@ -1,14 +1,14 @@
 module TheAuthHelper
 
-  def search_form_for(record = :q, options = {}, &block)
-    options[:css] ||= {
-      wrapper_all: 'inline field',
-      wrapper_input: 'field',
-      wrapper_submit: 'field',
-      submit: 'ui green button'
-    }
-
-    super
-  end
+  # def search_form_for(record = :q, options = {}, &block)
+  #   options[:css] ||= {
+  #     wrapper_all: 'inline field',
+  #     wrapper_input: 'field',
+  #     wrapper_submit: 'field',
+  #     submit: 'ui green button'
+  #   }
+  #
+  #   super
+  # end
 
 end
