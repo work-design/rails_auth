e0638eb3070b4b3a64dee15abed4280ae47e38b2
qinmingyuan <mingyuan0715@foxmail.com>
2017-05-18 10:40:29 +0800

    add db

---
+++ db/migrate/20170518023400_the_auth_init.rb
@@ -0,0 +1,29 @@
+class TheAuthInit < ActiveRecord::Migration[5.1]
+  def change
+
+    create_table :users do |t|
+      t.string :name, limit: 100
+      t.string :avatar
+      t.string :email, limit: 100
+      t.boolean :email_confirm, default: false
+      t.string :mobile, limit: 20
+      t.boolean :mobile_confirm, default: false
+      t.string :password_digest
+      t.datetime :last_login_at
+      t.string :last_login_ip
+      t.boolean :disabled, default: false
+      t.timestamps
+    end
+
+    create_table :verify_tokens do |t|
+      t.references :user
+      t.string :type, limit: 100
+      t.string :token
+      t.datetime :expired_at
+      t.integer :access_counter, default: 0
+      t.timestamps
+    end
+
+  end
+end
+
