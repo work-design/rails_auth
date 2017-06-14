10b3b8df205a264c04171bf41f05c3af4634ff31
qinmingyuan <mingyuan0715@foxmail.com>
2017-06-14 17:30:19 +0800

    update config

--- app/models/concerns/facebook_helper.rb
+++ app/models/concerns/facebook_helper.rb
@@ -1,12 +1,12 @@
 class FacebookHelper
-
+  attr_reader :strategy
 
   def initialize
-    @strategy = OmniAuth::Strategies::Facebook.new(nil, 'abc', 'efg')
+    @strategy = OmniAuth::Strategies::Facebook.new(nil, SETTING['f_key'], SETTING['f_secret'])
   end
 
-  def strategy
-     access_token: ''
+  def info(acc: xx)
+     access_token = 'xx'
   end
 
 
--- config/initializers/omniauth_facebook.rb
+++ config/initializers/omniauth_facebook.rb
@@ -1,4 +1,4 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :developer unless Rails.env.production?
-  provider :facebook, SETTING['facebook_key'], SETTING['facebook_secret'], redirect_uri: SETTING['facebook_redirect_uri'], scope: SETTING['facebook_scope']
+  provider :facebook, SETTING['f_key'], SETTING['f_secret']
 end
