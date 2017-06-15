eda2c752cadb4dd8685a53d039acf2cee161cf80
qinmingyuan <mingyuan0715@foxmail.com>
2017-06-15 13:47:50 +0800

    add require

--- config/initializers/omniauth_facebook.rb
+++ config/initializers/omniauth_facebook.rb
@@ -1,3 +1,4 @@
+require 'omniauth-facebook'
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :developer unless Rails.env.production?
   provider :facebook, SETTING['f_key'], SETTING['f_secret']
