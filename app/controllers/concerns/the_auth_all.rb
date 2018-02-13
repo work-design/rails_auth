386002b47f8058c5720dc2e33fdeca440a064720
qinmingyuan <mingyuan0715@foxmail.com>
2018-02-13 19:31:49 +0800

    mini code

--- app/controllers/concerns/the_auth_all.rb
+++ app/controllers/concerns/the_auth_all.rb
@@ -4,10 +4,6 @@ require 'the_auth_api'
 module TheAuthAll
   extend ActiveSupport::Concern
 
-  included do
-    helper_method :current_user
-  end
-
   def current_user
     return @current_user if @current_user
     @current_user = login_from_sesson || login_from_token
