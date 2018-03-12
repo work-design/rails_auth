0887714c89ac1940235ffa2c9403a31149c718a0
qinmingyuan <mingyuan0715@foxmail.com>
2018-03-12 23:38:27 +0800

    join method include password verify

---
+++ app/controllers/the_auth_api/join_controller.rb
@@ -0,0 +1,24 @@
+class TheAuthApi::JoinController < TheAuthApi::BaseController
+
+  def new
+    @user = User.new(user_params)
+    
+    
+  end
+  
+  def create
+    @user = User.new(user_params)
+    if @user.join(params)
+      login_as @user
+      redirect_back_or_default
+    else
+      render :new, error: @user.errors.full_messages
+    end
+  end
+
+  private
+  def user_params
+    params.fetch(:user, {}).permit(:name, :email, :mobile, :password, :password_confirmation)
+  end
+
+end
