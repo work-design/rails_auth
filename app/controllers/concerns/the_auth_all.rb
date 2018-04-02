8903089299a6fc87be3db3b46cd1741e7c240bb7
qinmingyuan <mingyuan0715@foxmail.com>
2018-04-02 10:13:28 +0800

    no use require

--- app/controllers/concerns/the_auth_all.rb
+++ app/controllers/concerns/the_auth_all.rb
@@ -1,14 +1,11 @@
-require 'the_auth_controller'
-require 'the_auth_api'
-
 module TheAuthAll
   extend ActiveSupport::Concern
-  
+
   included do
     include TheAuthApi
     include TheAuthController
   end
-  
+
   def current_user
     return @current_user if @current_user
     @current_user = login_from_sesson || login_from_token
