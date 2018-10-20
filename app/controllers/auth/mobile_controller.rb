3504bc2df6a7805d1fcbd8ffdce939c5273314e5
qinmingyuan <mingyuan0715@foxmail.com>
2018-10-20 13:31:30 +0800

    fix build user

--- app/controllers/auth/mobile_controller.rb
+++ app/controllers/auth/mobile_controller.rb
@@ -23,7 +23,6 @@ class Auth::MobileController < Auth::BaseController
       @mobile_token = @user.mobile_tokens.valid.find_by(token: params[:token])
     else
       @mobile_token = MobileToken.valid.find_by(token: params[:token], account: params[:mobile])
-      @user = @mobile_token.build_user if @mobile_token
     end
 
     if @mobile_token
