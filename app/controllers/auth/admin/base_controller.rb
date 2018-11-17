ddedbccdfe1bacf81591217eef6381379842de1d
qinmingyuan <mingyuan0715@foxmail.com>
2018-11-17 22:12:12 +0800

    remove unused form builder

--- app/controllers/auth/admin/base_controller.rb
+++ app/controllers/auth/admin/base_controller.rb
@@ -1,5 +1,3 @@
 class Auth::Admin::BaseController < RailsAuth.config.admin_class.constantize
-  default_form_builder 'AuthAdminFormBuilder'
-
 
 end
--- app/controllers/auth/base_controller.rb
+++ app/controllers/auth/base_controller.rb
@@ -1,7 +1,4 @@
 class Auth::BaseController < RailsAuth.config.app_class.constantize
   include RailsAuthController
-  default_form_builder 'AuthFromBuilder' do
-
-  end
 
 end
