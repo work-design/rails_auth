70c35809b9aad1f4572e088d84243b49acd5fced
qinmingyuan <mingyuan0715@foxmail.com>
2018-11-22 20:29:39 +0800

    mobile -> account

--- app/assets/javascripts/controllers/auth/join/token.js
+++ app/assets/javascripts/controllers/auth/join/token.js
@@ -1,4 +1,4 @@
 document.getElementById('mobile_confirm').addEventListener('click', function(e){
   var remind_link = new URL(this.href);
-  this.href = remind_link.pathname + '?mobile=' + document.getElementById('user_account').value
+  this.href = remind_link.pathname + '?account=' + document.getElementById('user_account').value
 });
