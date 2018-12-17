1559c0e7748e1c0471ee2759b20293e2e8f75de7
qinmingyuan <mingyuan0715@foxmail.com>
2018-12-17 10:12:24 +0800

    add tow methods

--- app/controllers/auth/api/mes_controller.rb
+++ app/controllers/auth/api/mes_controller.rb
@@ -1,26 +1,28 @@
 class Auth::Api::MesController < Auth::Api::BaseController
+  before_action :require_logon_from_token
 
   def show
-    render json: current_user.as_json(root: true, include: [:oauth_users], methods: [:avatar_url])
-  end
-
-  def edit
+    render json: @user.as_json(root: true, include: [:oauth_users], methods: [:avatar_url])
   end
 
   def update
-    if current_user.update(user_params)
-      render json: current_user.as_json(root: true, methods: [:avatar_url]), status: :created
+    if @user.update(user_params)
+      render json: @user.as_json(root: true, methods: [:avatar_url]), status: :created
     else
-      render json: current_user.errors, status: :unprocessable_entity
+      process_errors(@user)
     end
   end
 
   def destroy
-    @me.destroy
-    redirect_to mes_url, notice: 'Me was successfully destroyed.'
+    @user.destroy
+    redirect_to api_me_url, notice: 'Me was successfully destroyed.'
   end
 
   private
+  def set_user
+    @user = current_user
+  end
+
   def user_params
     params.fetch(:user, {}).permit(
       :name,
