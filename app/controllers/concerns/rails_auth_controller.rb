e5e1734946515c9f26518e01475553d42797eb8b
qinmingyuan <mingyuan0715@foxmail.com>
2019-01-15 14:58:02 +0800

    only support login form auth token

--- app/controllers/concerns/rails_auth_controller.rb
+++ app/controllers/concerns/rails_auth_controller.rb
@@ -1,21 +1,31 @@
+require 'jwt'
 module RailsAuthController
   extend ActiveSupport::Concern
-  include RailsAuthApi
 
   included do
     helper_method :current_user
     after_action :set_auth_token
   end
 
-  def api_request?
-    request.headers['Auth-Token'].present? || request.format.json?
-  end
+  def require_login(js_template: RailsAuth::Engine.root + 'app/views/auth/login/new.js.erb')
+    return if login_from_token
 
-  def require_login
     if api_request?
-      require_login_from_token
+      raise ActionController::UnauthorizedError
     else
-      require_login_from_session
+      if request.xhr?
+        @local = false
+        render file: js_template and return
+      else
+        @local = true
+      end
+
+      store_location
+      if params[:form_id]
+        redirect_to login_url(form_id: params[:form_id], login: params[:login])
+      else
+        redirect_to login_url
+      end
     end
   end
 
@@ -28,30 +38,10 @@ module RailsAuthController
     end
   end
 
-  def require_login_from_session(js_template: RailsAuth::Engine.root + 'app/views/auth/login/new.js.erb')
-    return if login_from_session
-
-    if request.xhr?
-      @local = false
-      render file: js_template and return
-    else
-      @local = true
-    end
-
-    store_location
-    if params[:form_id]
-      redirect_to login_url(form_id: params[:form_id], login: params[:login])
-    else
-      redirect_to login_url
-    end
-  end
-
-  def login_from_session
-    @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
-  end
-
   def login_as(user)
-    session[:user_id] = user.id
+    unless api_request?
+      session[:auth_token] = user.auth_token
+    end
     user.update(last_login_at: Time.now)
 
     logger.debug "Login as User #{user.id}"
@@ -60,7 +50,7 @@ module RailsAuthController
   end
 
   def logout
-    session.delete(:user_id)
+    session.delete(:auth_token)
     @current_user = nil
   end
 
@@ -76,6 +66,46 @@ module RailsAuthController
   def redirect_back_or_default(default = RailsAuth.config.default_return_path, **options)
     redirect_to session[:return_to] || default, **options
     session[:return_to] = nil
+  end
+
+  def login_from_token
+    auth_token = request.headers['Auth-Token'].presence || session[:auth_token]
+
+    return unless auth_token
+
+    if verify_auth_token
+      @access_token = AccessToken.find_by token: request.headers['Auth-Token']
+    end
+    if @access_token
+      @current_user ||= @access_token.user
+    end
+  end
+
+  private
+  def set_auth_token
+    if api_request?
+      headers['Auth-Token'] = @current_user.auth_token if @current_user
+    else
+      session[:auth_token] = user.auth_token
+    end
+  end
+
+  def verify_auth_token
+    token = request.headers['Auth-Token']
+    payload = JwtHelper.decode_without_verification(token)
+
+    return unless payload
+
+    begin
+      password_digest = User.find_by(id: payload['iss']).password_digest.to_s
+      JWT.decode(token, password_digest, true, 'sub' => 'auth', verify_sub: true, verify_expiration: false)
+    rescue => e
+      puts nil, e.full_message(highlight: true, order: :top)
+    end
+  end
+
+  def api_request?
+    request.headers['Auth-Token'].present? || request.format.json?
   end
 
 end
