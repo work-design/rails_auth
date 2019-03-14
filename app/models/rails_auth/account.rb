8fd430a647dfd9cbe4ce8352a432bbefc71e70d8
qinmingyuan <mingyuan0715@foxmail.com>
2019-03-14 21:55:00 +0800

    auto save account type

--- app/models/rails_auth/account.rb
+++ app/models/rails_auth/account.rb
@@ -1,5 +1,12 @@
 class Account < ApplicationRecord
   belongs_to :user
+  after_initialize if: :new_record? do
+    if self.account.include?('@')
+      self.type = 'EmailAccount'
+    else
+      self.type = 'MobileAccount'
+    end
+  end
 
   def can_login_organ?
     confirmed? && member&.organ_token
