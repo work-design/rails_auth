ee6f712888fb26e5d18c412c1470921f9aaab44b
qinmingyuan <mingyuan0715@foxmail.com>
2019-05-13 14:14:07 +0800

    fix err

--- app/assets/javascripts/controllers/auth/join/new_login.js
+++ app/assets/javascripts/controllers/auth/join/new_login.js
@@ -1,21 +1,26 @@
 document.getElementById('mobile_confirm').addEventListener('click', function(e){
-  var remind_link = new URL(this.href);
   var identity = document.getElementById('identity').value;
-  var countdown = 60;
-  var self = this;
+  if (/.+/.test(identity)) {
+    var countdown = 60;
+    var self = this;
 
-  this.href = remind_link.pathname + '?identity=' + identity;
-  this.classList.add('disabled');
-  self.innerText = '重新发送(' + countdown + ')';
+    this.href += '?identity=' + identity;
+    this.classList.add('disabled');
+    this.innerText = '重新发送(' + countdown + ')';
 
-  var timer = setInterval(function() {
-    countdown--;
-    if (countdown <= 0) {
-      self.classList.remove('disabled');
-      self.innerText = '获取验证码';
-      clearInterval(timer);
-    } else {
-      self.innerText = '重新发送(' + countdown + ')';
-    }
-  }, 1000, countdown, timer, self);
+    var timer = setInterval(function() {
+      countdown--;
+      if (countdown <= 0) {
+        self.classList.remove('disabled');
+        self.innerText = '获取验证码';
+        clearInterval(timer);
+      } else {
+        self.innerText = '重新发送(' + countdown + ')';
+      }
+    }, 1000, countdown, timer, self);
+  } else {
+    alert('wrong number!');
+    e.preventDefault();
+    e.stopImmediatePropagation();
+  }
 });
