2d9a2fc0ac6165e6387b864ea26fa9d6d6aeb41c
qinmingyuan <mingyuan0715@foxmail.com>
2019-05-13 12:33:55 +0800

    test mobile token

--- app/assets/javascripts/controllers/auth/join/new_login.js
+++ app/assets/javascripts/controllers/auth/join/new_login.js
@@ -1,16 +1,17 @@
 document.getElementById('mobile_confirm').addEventListener('click', function(e){
   var remind_link = new URL(this.href);
   var identity = document.getElementById('identity').value;
-  var countdown = 5;
+  var countdown = 60;
   var self = this;
-  this.href = remind_link.pathname + '?identity=' + identity;
 
-  this.setAttribute('disabled', true);
+  this.href = remind_link.pathname + '?identity=' + identity;
+  this.classList.add('disabled');
+  self.innerText = '重新发送(' + countdown + ')';
 
   var timer = setInterval(function() {
     countdown--;
     if (countdown <= 0) {
-      //self.removeAttribute('disabled');
+      self.classList.remove('disabled');
       self.innerText = '获取验证码';
       clearInterval(timer);
     } else {
