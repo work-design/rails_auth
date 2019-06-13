59c5a6ae6d3bb04353003507809627b3e05990aa
qinmingyuan <mingyuan0715@foxmail.com>
2019-06-13 19:47:08 +0800

    user filter

--- app/controllers/auth/admin/users_controller.rb
+++ app/controllers/auth/admin/users_controller.rb
@@ -4,8 +4,8 @@ class Auth::Admin::UsersController < Auth::Admin::BaseController
   def index
     q_params = {
       'created_at-desc': 2
-    }.with_indifferent_access
-    q_params.merge! params.permit(:name, :mobile, :email, 'last_login_at-desc')
+    }
+    q_params.merge! params.permit(:name, 'accounts.identity', 'last_login_at-desc')
     @users = User.with_attached_avatar.includes(:oauth_users, :accounts).default_where(q_params).page(params[:page])
   end
 
