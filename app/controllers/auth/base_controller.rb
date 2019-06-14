f9e91aa3f6e16b87c8209b83acb471b5199b1ea8
qinmingyuan <mingyuan0715@foxmail.com>
2019-06-14 10:13:44 +0800

    login in by account from module, easy to override

--- app/controllers/auth/base_controller.rb
+++ app/controllers/auth/base_controller.rb
@@ -1,5 +1,4 @@
 class Auth::BaseController < RailsAuth.config.app_controller.constantize
-  include RailsAuth::Controller
 
   def set_remote
     unless request.xhr? || params[:form_id]
@@ -7,22 +6,6 @@ class Auth::BaseController < RailsAuth.config.app_controller.constantize
     end
   end
 
-  def login_by_account(account)
-    unless api_request?
-      session[:auth_token] = account.auth_token
-    end
-    
-    if params[:oauth_user_id].present?
-      oauth_user = OauthUser.find params[:oauth_user_id]
-      oauth_user.account_id ||= account.id
-      oauth_user.save
-    end
-    account.user.update(last_login_at: Time.now)
-  
-    @current_account = account
-    @current_user = account.user
-    
-    logger.debug "  ==========> Login by account as user: #{account.user_id}"
-  end
+ 
 
 end
--- app/controllers/rails_auth/controller.rb
+++ app/controllers/rails_auth/controller.rb
@@ -91,6 +91,24 @@ module RailsAuth::Controller
     session.delete :return_to
   end
 
+  def login_by_account(account)
+    unless api_request?
+      session[:auth_token] = account.auth_token
+    end
+  
+    if params[:oauth_user_id].present?
+      oauth_user = OauthUser.find params[:oauth_user_id]
+      oauth_user.account_id ||= account.id
+      oauth_user.save
+    end
+    account.user.update(last_login_at: Time.now)
+  
+    @current_account = account
+    @current_user = account.user
+  
+    logger.debug "  ==========> Login by account as user: #{account.user_id}"
+  end
+
   private
   def set_auth_token
     return unless @current_account
