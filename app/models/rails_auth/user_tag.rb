e4cdf1899e374f4178e5e2cb4f11bc85c7437bac
qinmingyuan <mingyuan0715@foxmail.com>
2019-07-12 18:17:51 +0800

    add default values

--- app/models/rails_auth/user_tag.rb
+++ app/models/rails_auth/user_tag.rb
@@ -2,7 +2,7 @@ module RailsAuth::UserTag
   extend ActiveSupport::Concern
   included do
     attribute :name, :string
-    attribute :user_taggeds_count, :integer
+    attribute :user_taggeds_count, :integer, default: 0
   end
   
 end
