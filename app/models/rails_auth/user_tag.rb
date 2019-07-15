318bbf35910e5310002c84c224bcf412380085a0
qinmingyuan <mingyuan0715@foxmail.com>
2019-07-15 14:47:29 +0800

    show user filter by tags

--- app/models/rails_auth/user_tag.rb
+++ app/models/rails_auth/user_tag.rb
@@ -3,6 +3,9 @@ module RailsAuth::UserTag
   included do
     attribute :name, :string
     attribute :user_taggeds_count, :integer, default: 0
+    
+    has_many :user_taggeds, dependent: :destroy
+    has_many :users, through: :user_taggeds
   end
   
 end
