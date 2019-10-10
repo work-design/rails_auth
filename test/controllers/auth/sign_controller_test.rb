683f95443b26c2464b31d4b4edde12c0afedd3f7
qinmingyuan <mingyuan0715@foxmail.com>
2019-10-10 10:49:54 +0800

    add 2 test

--- test/controllers/auth/sign_controller_test.rb
+++ test/controllers/auth/sign_controller_test.rb
@@ -5,16 +5,25 @@ class Auth::JoinControllerTest < ActionDispatch::IntegrationTest
   setup do
   end
 
-  test 'new ok' do
-    get join_url
+  test 'sign ok' do
+    get sign_url
+    assert_response :success
+  end
+  
+  test 'post sign ok' do
+    post sign_url, params: { identity: 'test@work.design' }
     assert_response :success
   end
 
   test 'create ok' do
     create :email_token, account: 'mingyuan0715@foxmail.com', token: '111111'
     assert_difference('User.count') do
-      post join_url, params: { user: { account: 'mingyuan0715@foxmail.com', password: '111111' }, token: '111111' }
+      post join_url, params: { identity: 'mingyuan0715@foxmail.com', password: '111111', token: '111111' }
     end
+  end
+  
+  test 'logout ok' do
+  
   end
 
 end
