348ac6dbff484419b464862b62ee4e03880e4062
qinmingyuan <mingyuan0715@foxmail.com>
2019-10-20 21:00:27 +0800

    fixed some err

--- app/controllers/auth/mine/users_controller.rb
+++ app/controllers/auth/mine/users_controller.rb
@@ -1,5 +1,4 @@
 class Auth::Mine::UsersController < Auth::Mine::BaseController
-  before_action :set_user
 
   def show
   end
@@ -8,32 +7,18 @@ class Auth::Mine::UsersController < Auth::Mine::BaseController
   end
 
   def update
-    @user.assign_attributes user_params
-  
-    respond_to do |format|
-      if @user.save
-        format.js
-        format.html { redirect_to my_user_url }
-        format.json {
-          render json: { user: @user.as_json, filename: url_for(@user.avatar) }
-        }
-      else
-        format.html { render action: 'edit' }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
-      end
+    current_user.assign_attributes user_params
+    
+    unless current_user.save
+      render :edit, locals: { model: current_user }, status: :unprocessable_entity
     end
   end
 
   def destroy
     @user.destroy
-    redirect_to api_me_url
   end
 
   private
-  def set_user
-    @user = current_user
-  end
-
   def user_params
     params.fetch(:user, {}).permit(
       :name,
