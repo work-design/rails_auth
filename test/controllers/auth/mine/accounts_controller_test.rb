0135b75a49dc38f0fdbc408ab171b967bf9edcfb
qinmingyuan <mingyuan0715@foxmail.com>
2019-10-23 20:15:31 +0800

    clean some files

--- test/controllers/auth/mine/accounts_controller_test.rb
+++ test/controllers/auth/mine/accounts_controller_test.rb
@@ -19,20 +19,25 @@ class Auth::Mine::AccountsControllerTest < ActionDispatch::IntegrationTest
 
   test 'create ok' do
     assert_difference('Account.count') do
-      post my_accounts_url, params: { account: { identity: 'test1@work.design' } }
+      post my_accounts_url, params: { account: { identity: 'test1@work.design' } }, xhr: true
     end
 
     assert_response :success
   end
+  
+  test 'edit_confirm ok' do
+    get confirm_my_account_url(@account)
+    assert_response :success
+  end
 
   test 'update ok' do
-    patch my_account_url(@account), params: { account: { identity: 'test1@work.design', confirmed: true, primary: true } }
+    patch my_account_url(@account), params: { account: { confirmed: true, primary: true } }
     assert_response :success
   end
 
   test 'destroy ok' do
     assert_difference('Account.count', -1) do
-      delete my_account_url(@account)
+      delete my_account_url(@account), xhr: true
     end
 
     assert_response :success
