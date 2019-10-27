6423d5b149c55ba46d3665aeee5503539acb8ae7
qinmingyuan <mingyuan0715@foxmail.com>
2019-10-27 11:29:40 +0800

    auth as token

--- db/migrate/20170518023400_rails_auth_init.rb
+++ db/migrate/20170518023400_rails_auth_init.rb
@@ -37,11 +37,10 @@ class RailsAuthInit < ActiveRecord::Migration[5.1]
       t.references :user
       t.references :oauth_user
       t.references :account
-      t.string :token
+      t.string :token, index: { unique: true }
       t.datetime :expire_at
       t.string :session_key
       t.integer :access_counter, default: 0
-      t.index [:user_id, :oauth_user_id, :account_id, :token], unique: true, name: 'index_authorized_tokens_on_unique_token'
       t.timestamps
     end
 
