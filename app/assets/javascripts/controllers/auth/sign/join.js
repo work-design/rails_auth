5fd4f6aed2c432f910688d95770c9d6541998f0b
manxingxing <manxingxing@gmail.com>
2019-11-07 16:28:07 +0800

    注册时需提供密码
    目前没有其他设置密码的地方

--- app/assets/javascripts/controllers/auth/sign/join.js
+++ app/assets/javascripts/controllers/auth/sign/join.js
@@ -1,1 +1,24 @@
 import './login'
+
+$('.ui.form').form({
+    fields: {
+      password: {
+          rules: [
+              { type: 'empty', prompt: '密码不能为空' },
+          ]
+      },
+      password_confirmation: {
+          rules: [
+              { type: 'match[password]', prompt: '两次输入不同' },
+          ]
+      },
+      token: {
+          rules: [
+              { type: 'empty', prompt: '验证码不能为空' }
+          ]
+      }
+    },
+    revalidate: true,
+    preventLeaving: true
+  })
+;
