cf4f9b02fa101fd7985468f3433e4fd28f4bb2f4
qinmingyuan <mingyuan0715@foxmail.com>
2019-11-17 20:56:35 +0800

    use bin/yarn;

--- test/controllers/auth/admin/user_tags_controller_test.rb
+++ test/controllers/auth/admin/user_tags_controller_test.rb
@@ -12,13 +12,13 @@ class Auth::Admin::UserTagsControllerTest < ActionDispatch::IntegrationTest
   end
 
   test 'new ok' do
-    get new_admin_user_tag_url
+    get new_admin_user_tag_url, xhr: true
     assert_response :success
   end
 
   test 'create ok' do
     assert_difference('UserTag.count') do
-      post admin_user_tags_url, params: { user_tag: { name: 'good' } }
+      post admin_user_tags_url, params: { user_tag: { name: 'good' } }, xhr: true
     end
 
     assert_response :success
@@ -30,18 +30,21 @@ class Auth::Admin::UserTagsControllerTest < ActionDispatch::IntegrationTest
   end
 
   test 'edit ok' do
-    get edit_admin_user_tag_url(@user_tag)
+    get edit_admin_user_tag_url(@user_tag), xhr: true
     assert_response :success
   end
 
   test 'update ok' do
-    patch admin_user_tag_url(@user_tag), params: { user_tag: { name: 'good' } }
+    patch admin_user_tag_url(@user_tag), params: { user_tag: { name: 'good' } }, xhr: true
+    
+    @user_tag.reload
+    assert_equal 'good', @user_tag.name
     assert_response :success
   end
 
   test 'destroy ok' do
     assert_difference('UserTag.count', -1) do
-      delete admin_user_tag_url(@user_tag)
+      delete admin_user_tag_url(@user_tag), xhr: true
     end
 
     assert_response :success
