083485324eee3fdbe17e1a32eb870d93a113403d
qinmingyuan <mingyuan0715@foxmail.com>
2019-11-30 20:46:37 +0800

    fix testes

--- app/models/rails_auth/user_tag.rb
+++ app/models/rails_auth/user_tag.rb
@@ -3,10 +3,9 @@ module RailsAuth::UserTag
   included do
     attribute :name, :string
     attribute :user_taggeds_count, :integer, default: 0
-    attribute :user_tagging, :string
 
     belongs_to :organ, optional: true
-    belongs_to :tagging, polymorphic: true
+    belongs_to :tagging, polymorphic: true, optional: true
     has_many :user_taggeds, dependent: :destroy
   end
   
--- test/factories/user_tags.rb
+++ test/factories/user_tags.rb
@@ -1,12 +1,5 @@
 FactoryBot.define do
   factory :user_tag do
-    id { 1 }
-    organ_id { 1 }
-    tagging_type { "MyString" }
-    tagging_id { 1 }
-    name { "MyString" }
-    user_taggeds_count { 1 }
-    created_at { "2019-09-04 17:45:41" }
-    updated_at { "2019-09-04 17:45:41" }
+    name { 'MyString' }
   end
 end
