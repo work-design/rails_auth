f49f1d0703adf2b7742e11c2f49fb983710f8cef
qinmingyuan <mingyuan0715@foxmail.com>
2020-04-17 21:30:35 +0800

    fix test

--- test/controllers/auth/panel/accounts_controller_test.rb
+++ test/controllers/auth/panel/accounts_controller_test.rb
@@ -1,49 +1,49 @@
 require 'test_helper'
 
-class Auth::Admin::AccountsControllerTest < ActionDispatch::IntegrationTest
-  
+class Auth::Panel::AccountsControllerTest < ActionDispatch::IntegrationTest
+
   setup do
     @account = create :account
   end
 
   test 'index ok' do
-    get admin_accounts_url
+    get panel_accounts_url
     assert_response :success
   end
 
   test 'new ok' do
-    get new_admin_account_url, xhr: true
+    get new_panel_account_url, xhr: true
     assert_response :success
   end
 
   test 'create ok' do
     Account.delete_all
-    
+
     assert_difference('Account.count') do
-      post admin_accounts_url, params: { account: { identity: 'test@work.design' } }, xhr: true
+      post panel_accounts_url, params: { account: { identity: 'test@work.design' } }, xhr: true
     end
 
     assert_response :success
   end
 
   test 'show ok' do
-    get admin_account_url(@account), xhr: true
+    get panel_account_url(@account), xhr: true
     assert_response :success
   end
 
   test 'edit ok' do
-    get edit_admin_account_url(@account), xhr: true
+    get edit_panel_account_url(@account), xhr: true
     assert_response :success
   end
 
   test 'update ok' do
-    patch admin_account_url(@account), params: { identity: 'test_update@work.design' }, xhr: true
+    patch panel_account_url(@account), params: { identity: 'test_update@work.design' }, xhr: true
     assert_response :success
   end
 
   test 'destroy ok' do
     assert_difference('Account.count', -1) do
-      delete admin_account_url(@account), xhr: true
+      delete panel_account_url(@account), xhr: true
     end
 
     assert_response :success
--- test/controllers/auth/panel/authorized_tokens_controller_test.rb
+++ test/controllers/auth/panel/authorized_tokens_controller_test.rb
@@ -1,28 +1,28 @@
 require 'test_helper'
 
-class Auth::Admin::AuthorizedTokensControllerTest < ActionDispatch::IntegrationTest
+class Auth::Panel::AuthorizedTokensControllerTest < ActionDispatch::IntegrationTest
   setup do
     @authorized_token = create :authorized_token
   end
 
   test 'index ok' do
-    get admin_authorized_tokens_url
+    get panel_authorized_tokens_url
     assert_response :success
   end
 
   test 'edit ok' do
-    get edit_admin_authorized_token_url(@authorized_token), xhr: true
+    get edit_panel_authorized_token_url(@authorized_token), xhr: true
     assert_response :success
   end
 
   test 'update ok' do
-    patch admin_authorized_token_url(@authorized_token), params: { authorized_token: { expire_at: Time.now } }, xhr: true
+    patch panel_authorized_token_url(@authorized_token), params: { authorized_token: { expire_at: Time.now } }, xhr: true
     assert_response :success
   end
 
   test 'destroy ok' do
     assert_difference('AuthorizedToken.count', -1) do
-      delete admin_authorized_token_url(@authorized_token), xhr: true
+      delete panel_authorized_token_url(@authorized_token), xhr: true
     end
 
     assert_response :success
