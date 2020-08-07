5f82d0947fcdfaff49b524accb8f583133cd5f22
qinmingyuan <mingyuan0715@foxmail.com>
2020-08-07 14:46:40 +0800

    filter by appids

--- app/controllers/auth/admin/oauth_users_controller.rb
+++ app/controllers/auth/admin/oauth_users_controller.rb
@@ -3,7 +3,7 @@ class Auth::Admin::OauthUsersController < Auth::Admin::BaseController
 
   def index
     q_params = {}
-    q_params.merge! app_id: current_organ.wechat_apps.pluck(:appid) if defined?(current_organ) && current_organ
+    q_params.merge! app_id: current_session_organ.wechat_apps.pluck(:appid) if defined?(current_session_organ) && current_session_organ
     q_params.merge! params.permit(:user_id, :uid, :app_id, :name)
     @oauth_users = OauthUser.default_where(q_params).order(id: :desc).page(params[:page])
   end
@@ -13,7 +13,7 @@ class Auth::Admin::OauthUsersController < Auth::Admin::BaseController
 
   def update
     @oauth_user.assign_attributes(oauth_user_params)
-    
+
     unless @oauth_user.save
       render :edit, locals: { model: @oauth_user }, status: :unprocessable_entity
     end
