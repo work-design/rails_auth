17c1f8b78f105a6d01bfb0586c3b257a2fe3ef46
qinmingyuan <mingyuan0715@foxmail.com>
2020-09-10 23:13:57 +0800

    no more mock organ

--- app/models/rails_auth/authorized_token.rb
+++ app/models/rails_auth/authorized_token.rb
@@ -12,7 +12,6 @@ module RailsAuth::AuthorizedToken
     attribute :expire_at, :datetime
     attribute :session_key, :string, comment: '目前在小程序下用到'
     attribute :access_counter, :integer, default: 0
-    attribute :mock_organ, :boolean, default: false
     attribute :mock_member, :boolean, default: false
     attribute :mock_user, :boolean, default: false
 
