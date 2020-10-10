f7998e42dd89be5ab4ea9858c3bef266a9437474
MingyuanQin <mingyuan0715@foxmail.com>
2020-10-10 09:45:46 +0800

    add as

--- app/models/rails_auth/user_tagged.rb
+++ app/models/rails_auth/user_tagged.rb
@@ -4,6 +4,7 @@ module RailsAuth::UserTagged
   included do
     belongs_to :user_tag, counter_cache: true
     belongs_to :tagged, polymorphic: true, optional: true
+    has_many :user_annunciates, foreign_key: :user_tag_id, primary_key: :user_tag_id
   end
 
 end
