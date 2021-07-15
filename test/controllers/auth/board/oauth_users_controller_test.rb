518cd4103cc22974a46a211c579dff16af3c69de
MingyuanQin <mingyuan0715@foxmail.com>
2021-07-16 00:24:45 +0800

    board

--- test/controllers/auth/board/oauth_users_controller_test.rb
+++ test/controllers/auth/board/oauth_users_controller_test.rb
@@ -2,20 +2,20 @@ require 'test_helper'
 class Auth::Board::OauthUsersControllerTest < ActionDispatch::IntegrationTest
 
   setup do
-    account = create :account
-    post '/login', params: { identity: account.identity, password: account.user.password }
+    account = auth_accounts(:one)
+    post url_for(controller: 'auth/sign', action: 'login'), params: { identity: account.identity, password: 'secret' }, as: :turbo_stream
 
-    @oauth_user = create :oauth_user
+    @oauth_user = auth_oauth_users(:one)
   end
 
   test 'index ok' do
-    get my_oauth_users_url
+    get url_for(controller: 'auth/board/oauth_users')
     assert_response :success
   end
 
   test 'destroy ok' do
-    assert_difference('OauthUser.count', -1) do
-      delete my_oauth_user_url(@oauth_user)
+    assert_difference('Auth::OauthUser.count', -1) do
+      delete url_for(controller: 'auth/board/oauth_users', action: 'destroy', id: @oauth_user.id), as: :turbo_stream
     end
 
     assert_response :success
--- test/controllers/auth/board/users_controller_test.rb
+++ test/controllers/auth/board/users_controller_test.rb
@@ -3,33 +3,32 @@ require 'test_helper'
 class Auth::Board::UsersControllerTest < ActionDispatch::IntegrationTest
 
   setup do
-    @account = create :account
+    @account = auth_accounts(:one)
     @user = @account.user
 
-    post '/login', params: { identity: @account.identity, password: @user.password }
+    post url_for(controller: 'auth/sign', action: 'login'), params: { identity: @account.identity, password: 'secret' }, as: :turbo_stream
   end
 
   test 'show ok' do
-    get my_user_url
+    get url_for(controller: 'auth/board/users', action: 'show')
     assert_response :success
   end
 
   test 'edit ok' do
-    get edit_my_user_url
+    get url_for(controller: 'auth/board/users', action: 'edit')
     assert_response :success
   end
 
   test 'update ok' do
-    patch my_user_url,
-          params: { user: { name: 'a@b.c' } }
+    patch url_for(controller: 'auth/board/users', action: 'update'), params: { user: { name: 'a@b.c' } }, as: :turbo_stream
     @user.reload
     assert_equal 'a@b.c', @user.name
     assert_response :success
   end
 
   test 'destroy ok' do
-    assert_difference('User.count', -1) do
-      delete my_user_url
+    assert_difference('Auth::User.count', -1) do
+      delete url_for(controller: 'auth/board/users', action: 'destroy'), as: :turbo_stream
     end
 
     assert_response :success
