ca5a8845512333e67822be951a6f4361bcd0589e
MingyuanQin <mingyuan0715@foxmail.com>
2021-07-16 00:12:07 +0800

    fix authorized token

--- test/controllers/auth/panel/authorized_tokens_controller_test.rb
+++ test/controllers/auth/panel/authorized_tokens_controller_test.rb
@@ -2,27 +2,27 @@ require 'test_helper'
 
 class Auth::Panel::AuthorizedTokensControllerTest < ActionDispatch::IntegrationTest
   setup do
-    @authorized_token = create :authorized_token
+    @authorized_token = auth_authorized_tokens(:one)
   end
 
   test 'index ok' do
-    get panel_authorized_tokens_url
+    get url_for(controller: 'auth/panel/authorized_tokens')
     assert_response :success
   end
 
   test 'edit ok' do
-    get edit_panel_authorized_token_url(@authorized_token), xhr: true
+    get url_for(controller: 'auth/panel/authorized_tokens', action: 'edit', id: @authorized_token.id)
     assert_response :success
   end
 
   test 'update ok' do
-    patch panel_authorized_token_url(@authorized_token), params: { authorized_token: { expire_at: Time.now } }, xhr: true
+    patch url_for(controller: 'auth/panel/authorized_tokens', action: 'update', id: @authorized_token.id), params: { authorized_token: { expire_at: Time.now } }, as: :turbo_stream
     assert_response :success
   end
 
   test 'destroy ok' do
-    assert_difference('AuthorizedToken.count', -1) do
-      delete panel_authorized_token_url(@authorized_token), xhr: true
+    assert_difference('Auth::AuthorizedToken.count', -1) do
+      delete url_for(controller: 'auth/panel/authorized_tokens', action: 'destroy', id: @authorized_token.id), as: :turbo_stream
     end
 
     assert_response :success
