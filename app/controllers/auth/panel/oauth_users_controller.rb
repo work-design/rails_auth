a6662875a112a1260353f20af8b71e95247ec94b
qinmingyuan <mingyuan0715@foxmail.com>
2022-10-07 09:53:26 +0800

    auth user

--- app/controllers/auth/panel/oauth_users_controller.rb
+++ app/controllers/auth/panel/oauth_users_controller.rb
@@ -5,7 +5,7 @@ module Auth
       q_params = {}
       q_params.merge! params.permit(:identity, :uid, :unionid, :appid, :name)
 
-      @oauth_users = OauthUser.includes(:user).default_where(q_params).order(id: :desc).page(params[:page])
+      @oauth_users = OauthUser.includes(:user, :app).default_where(q_params).order(id: :desc).page(params[:page])
     end
 
     private
