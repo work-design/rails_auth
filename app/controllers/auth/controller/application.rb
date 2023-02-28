097650d301c0eb969fce20076736c0579c964cbd
qinmingyuan <mingyuan0715@foxmail.com>
2023-02-28 21:55:46 +0800

    layout

--- app/controllers/auth/controller/application.rb
+++ app/controllers/auth/controller/application.rb
@@ -20,9 +20,9 @@ module Auth
       end
 
       if request.variant.include?(:mini_program)
-        render 'require_program_login', locals: { url: url_for(return_hash) }
+        render 'require_program_login', layout: 'raw', locals: { url: url_for(return_hash) }
       else
-        render 'require_user', locals: { url: url_for(controller: '/auth/sign', action: 'sign', identity: params[:identity]) }
+        render 'require_user', layout: 'raw', locals: { url: url_for(controller: '/auth/sign', action: 'sign', identity: params[:identity]) }
       end
     end
 
