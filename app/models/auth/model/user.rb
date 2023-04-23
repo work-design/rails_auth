c8ccbf8a61e506f8b6426328be6d9fae35cb9ef3
qinmingyuan <mingyuan0715@foxmail.com>
2023-04-23 18:31:52 +0800

    fix account

--- app/models/auth/model/user.rb
+++ app/models/auth/model/user.rb
@@ -24,7 +24,7 @@ module Auth
       has_many :accounts, inverse_of: :user, dependent: :nullify
       has_many :verify_tokens, through: :accounts
       has_many :authorized_tokens, through: :accounts
-      has_many :oauth_users, through: :accounts
+      has_many :oauth_users
       has_many :confirmed_accounts, -> { where(confirmed: true) }, class_name: 'Account'
       accepts_nested_attributes_for :accounts
 
