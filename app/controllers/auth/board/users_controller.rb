faf43381b7a04e0d37293bf52ed30f62e7a0561f
qinmingyuan <mingyuan0715@foxmail.com>
2023-08-19 23:05:33 +0800

    simple json

--- app/controllers/auth/board/users_controller.rb
+++ app/controllers/auth/board/users_controller.rb
@@ -2,26 +2,6 @@ module Auth
   class Board::UsersController < Board::BaseController
     before_action :set_user
 
-    def show
-    end
-
-    def edit
-    end
-
-    def update
-      current_user.assign_attributes user_params
-
-      if current_user.save
-        render 'update', locals: { return_to: params[:return_to].presence }
-      else
-        render :edit, locals: { model: current_user }, status: :unprocessable_entity
-      end
-    end
-
-    def destroy
-      current_user.destroy
-    end
-
     private
     def set_user
       @user = current_user
