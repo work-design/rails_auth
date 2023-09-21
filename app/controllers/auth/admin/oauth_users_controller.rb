433637126a9507068ec522636335aa918f73a15c
qinmingyuan <mingyuan0715@foxmail.com>
2023-09-21 18:48:40 +0800

    filter by appid

--- app/controllers/auth/admin/oauth_users_controller.rb
+++ app/controllers/auth/admin/oauth_users_controller.rb
@@ -3,7 +3,9 @@ module Auth
     before_action :set_oauth_user, only: [:show, :update, :destroy]
 
     def index
-      q_params = {}
+      q_params = {
+        appid: current_organ.apps.pluck(:appid)
+      }
       q_params.merge! params.permit(:user_id, :uid, :appid, :name)
 
       @oauth_users = OauthUser.default_where(q_params).order(id: :desc).page(params[:page])
