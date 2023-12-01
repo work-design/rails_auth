316c6951b3c52d4ce5e444d7460ccdacae8be020
qinmingyuan <mingyuan0715@foxmail.com>
2023-12-01 20:30:44 +0800

    support wechat

--- app/models/auth/authorized_token.rb
+++ app/models/auth/authorized_token.rb
@@ -2,5 +2,6 @@ module Auth
   class AuthorizedToken < ApplicationRecord
     include Model::AuthorizedToken
     include Org::Ext::AuthorizedToken if defined? RailsOrg
+    include Wechat::Ext::AuthorizedToken if defined? RailsWechat
   end
 end
