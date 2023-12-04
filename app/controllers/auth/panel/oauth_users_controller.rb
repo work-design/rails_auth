022f4d3567f6e1d9bbac8912a7a6ffb9ac1e02dc
qinmingyuan <mingyuan0715@foxmail.com>
2023-12-04 08:45:29 +0800

    code clean

--- app/controllers/auth/panel/oauth_users_controller.rb
+++ app/controllers/auth/panel/oauth_users_controller.rb
@@ -9,9 +9,7 @@ module Auth
     end
 
     def month
-      q_params = {
-        #good_type: ['Ship::BoxHost', 'Ship::BoxSell']
-      }
+      q_params = {}
       x = Arel.sql("date_trunc('day', created_at, '#{Time.zone.tzinfo.identifier}')")
       r = OauthUser.where(q_params).group(x).order(x).count
 
