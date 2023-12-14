1e38a5168f069844577fecd767800cca24913568
qinmingyuan <mingyuan0715@foxmail.com>
2023-12-14 14:42:27 +0800

    auth account

--- app/models/auth/account.rb
+++ app/models/auth/account.rb
@@ -2,5 +2,8 @@ module Auth
   class Account < ApplicationRecord
     include Model::Account
     include Org::Ext::Account if defined? RailsOrg
+    include Wechat::Ext::Account if defined? RailsWechat
+    include Profiled::Ext::Account if defined? RailsProfiled
+    include Crm::Ext::Account if defined? RailsCrm
   end
 end
