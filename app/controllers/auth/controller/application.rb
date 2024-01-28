726b7c364156d9c1649fca6f88c838895f2ef524
qinmingyuan <mingyuan0715@foxmail.com>
2024-01-28 22:25:59 +0800

    authjwt token

--- app/controllers/auth/controller/application.rb
+++ app/controllers/auth/controller/application.rb
@@ -15,7 +15,7 @@ module Auth
     end
 
     def check_jwt_token
-      @current_authorized_token = AuthorizedToken.find_or_create_by(jwt_token: params[:auth_jwt_token])
+      @current_authorized_token = AuthorizedToken.find_or_create_by(encrypted_token: params[:auth_jwt_token])
       @current_user = current_authorized_token&.user
     end
 
