a8055004a8d04c21b414083fb7d4a5b3b42c4c80
qinmingyuan <mingyuan0715@foxmail.com>
2025-02-26 17:41:55 +0800

    showw app name

--- app/controllers/auth/panel/users_controller.rb
+++ app/controllers/auth/panel/users_controller.rb
@@ -8,11 +8,7 @@ module Auth
       }
       q_params.merge! user_filter_params
 
-      @users = User.with_attached_avatar.includes(
-        :accounts,
-        :roles,
-        oauth_users: :app
-      ).default_where(q_params).page(params[:page])
+      @users = User.with_attached_avatar.includes(:accounts, :roles, :oauth_users).default_where(q_params).page(params[:page])
     end
 
     def month
