91575348620cd5a18240abfc794778b036b34eb8
qinmingyuan <mingyuan0715@foxmail.com>
2025-05-28 10:17:03 +0800

    use logicn

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
         get :bind
         post :direct
         post :join
-        get :login_new
+        get 'login' => :login_new
         post :login
         post :token_login
         post :token
