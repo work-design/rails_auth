c6389e49d22525d36f8ad1d6b54a6a133eef5c56
qinmingyuan <mingyuan0715@foxmail.com>
2025-06-29 00:12:25 +0800

    org organ

--- app/models/auth/model/user_tag.rb
+++ app/models/auth/model/user_tag.rb
@@ -6,7 +6,7 @@ module Auth
       attribute :name, :string
       attribute :user_taggeds_count, :integer, default: 0
 
-      belongs_to :organ, optional: true
+      belongs_to :organ, class_name: 'Org::Organ', optional: true
       belongs_to :user_tagging, polymorphic: true, optional: true
 
       has_many :user_taggeds, dependent: :destroy_async
